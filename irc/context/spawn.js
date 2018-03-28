a8c3abfc3c2c549da7e65f8bb0e28e1588da8c0c
kirjavascript <snkenjoi@gmail.com>
2018-03-28 23:06:25 +0100

    added ping

--- irc/context/spawn.js
+++ irc/context/spawn.js
@@ -1,17 +1,17 @@
 const { spawn } = require('child_process');
 
-function createCommand(...args) {
-    const cmd = spawn(...args);
+function createCommand(exe, params, outFilter = (d) => d, errFilter = (d) => d) {
+    const cmd = spawn(exe, params);
     return new Promise((resolve, reject) => {
         let [out, err] = ['', ''];
         cmd.stdout.on('data', data => out += data);
         cmd.stderr.on('data', data => err += data);
         cmd.on('close', (code) => {
             if (code == 0) {
-                resolve(out);
+                resolve(outFilter(out));
             }
             else {
-                reject(err);
+                reject(errFilter(err));
             }
         });
     });
