037f096f7855804c84480d10f07899c929e8bba7
kirjavascript <snkenjoi@gmail.com>
2018-03-28 23:36:13 +0100

    add auth markup

---
+++ web/modules/auth/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+
+import { env } from '#store';
+
+@observer
+export class Auth extends Component {
+
+    state = {
+        password: '',
+    };
+
+    onChange = (e) => {
+        this.setState({password: e.target.value});
+    };
+
+    render() {
+        return do {
+            if (env.admin) {
+                <div className="login">
+                    ~admin mode~
+                </div>
+            }
+            else {
+                <input
+                    type="password"
+                    value={this.state.password}
+                    className="login tr"
+                    placeholder="admin login"
+                    onChange={this.onChange}
+                />
+            }
+        };
+    }
+}
