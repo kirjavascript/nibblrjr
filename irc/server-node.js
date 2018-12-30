3cb14374e25900304c64be37cef00cffbd2aa9c2
kirjavascript <snkenjoi@gmail.com>
2018-12-30 01:27:49 +0000

    fixed for freenode

--- irc/server-node.js
+++ irc/server-node.js
@@ -28,7 +28,7 @@ class ServerNode {
             floodProtection: this.get('floodProtection', true),
             floodProtectionDelay: this.get('floodProtectionDelay', 250),
             autoRejoin: true,
-            // debug: true,
+            debug: true,
         });
 
         this.timeouts = [];
