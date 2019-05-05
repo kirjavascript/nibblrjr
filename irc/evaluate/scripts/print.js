71e84239a149c76b5a5153a5dc5d92ce777b2fac
kirjavascript <snkenjoi@gmail.com>
2019-05-05 21:48:22 +0100

    printing complete

--- irc/evaluate/scripts/print.js
+++ irc/evaluate/scripts/print.js
@@ -1,23 +1,37 @@
-const { objectDebug } = require('./evaluate');
-const { parseColors, notify } = require('./evaluate/scripts/colors');
+module.exports = { createSend };
 
-const messageFactory = (type, node, msgData, canBroadcast = false) => {
-    const { client } = node;
-    const { target: defaultTarget, isPM } = msgData;
+// TODO: createSendHost
+
+function createSend(config) {
+    return {
+        print: messageFactory('say', config),
+        notice: messageFactory('notice', config),
+        action: messageFactory('action', config),
+    };
+}
+
+function messageFactory(type, {
+    hasColors,
+    canBroadcast = false,
+    lineLimit = 10,
+    message,
+    colors,
+    inspect,
+    sendRaw,
+}) {
+    const { target: defaultTarget, isPM } = message;
     let count = 0;
-    const limit = node.getChannelConfig(msgData.to).lineLimit || (isPM ? 50 : 10);
 
     // raw
-    const sendRaw = (text, { target = defaultTarget, log = true } = {}) => {
+    const sendBase = (text, { target = defaultTarget, log = true } = {}) => {
         if (!canBroadcast && target !== defaultTarget) {
             throw new Error('cannot broadcast');
         }
-        // only send if correctly connected to server and not to services
-        if (!node.registered || String(target).toLowerCase().includes('serv')) return;
+        if (String(target).toLowerCase().includes('serv')) return;
         if (typeof text != 'string') {
             text = String(text);
         }
-        if (!node.get('colors', true)) {
+        if (!hasColors) {
             text = text.replace(/(\x03\d{0,2}(,\d{0,2}|\x02\x02)?|\x0f|\x07|\x1D|\x02|\x1f)/g, '');
         }
         // strip out \r, fixes; print.raw(`${String.fromCharCode(13)}QUIT`)
@@ -27,16 +41,16 @@ const messageFactory = (type, node, msgData, canBroadcast = false) => {
             .map(line => line.match(/.{1,400}/g))
             .forEach((lines) => {
                 lines && lines.forEach(line => {
-                    if (++count <= limit) {
-                        client[type](target, line);
+                    if (++count <= lineLimit) {
+                        sendRaw(type, target, line);
                     }
                 });
             });
 
-        if (count > limit) return;
+        if (count > lineLimit) return;
 
         // log to DB
-        if (!msgData.isPM && log) {
+        if (!isPM && log) {
             // lag a little so messages are the right order
             setTimeout(() => {
                 node.database.log({
@@ -51,42 +65,25 @@ const messageFactory = (type, node, msgData, canBroadcast = false) => {
 
     // colours
     const send = (text, config) => {
-        return sendRaw(parseColors(text), config);
+        return sendBase(colors(text), config);
     };
 
-    send.raw = sendRaw;
+    send.raw = sendBase;
 
     // inspect
     send.log = (text, config = {}) => {
         const { depth, colors, truncate, ...otherConfig } = config;
-        return sendRaw(objectDebug(text, {depth, colors, truncate}), otherConfig);
+        return sendBase(inspect(text, {depth, colors, truncate}), otherConfig);
     };
     send.error = (error, config) => {
-        return send(notify.error(error), config);
+        return send(colors.error(error), config);
     };
     send.info = (text, config) => {
-        return send(notify.info(text), config);
+        return send(colors.info(text), config);
     };
     send.success = (text, config) => {
-        return send(notify.success(text), config);
+        return send(colors.success(text), config);
     };
 
     return send;
-};
-
-const printFactory = (...args) => {
-    return messageFactory('say', ...args);
-};
-const noticeFactory = (...args) => {
-    return messageFactory('notice', ...args);
-};
-const actionFactory = (...args) => {
-    return messageFactory('action', ...args);
-};
-
-
-module.exports = {
-    printFactory,
-    noticeFactory,
-    actionFactory,
-};
+}
