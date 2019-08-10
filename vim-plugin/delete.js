87aa8b5867a8f1e3bf27e3d91a2b549727a81b64
kirjavascript <snkenjoi@gmail.com>
2019-08-11 00:17:04 +0100

    supported deleting commands

---
+++ vim-plugin/delete.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const { getCommand, deleteCommand } = require('../database/commands');
+const stdin = fs.readFileSync(0, 'utf-8').toString();
+
+const { name, locked } = getCommand(stdin);
+if (locked) {
+    console.error(`${name} is locked`);
+    process.exit(1);
+} else {
+    deleteCommand(name);
+}
