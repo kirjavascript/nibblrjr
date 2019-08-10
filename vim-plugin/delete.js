12f3ddcf7546e43ebc45d8d6615bcaaa687da510
kirjavascript <snkenjoi@gmail.com>
2019-08-11 00:49:37 +0100

    allowed editing locked commands

--- vim-plugin/delete.js
+++ vim-plugin/delete.js
@@ -3,9 +3,9 @@ const { getCommand, deleteCommand } = require('../database/commands');
 const stdin = fs.readFileSync(0, 'utf-8').toString();
 
 const { name, locked } = getCommand(stdin);
-if (locked) {
-    console.error(`${name} is locked`);
-    process.exit(1);
-} else {
+// if (locked) {
+//     console.error(`${name} is locked`);
+//     process.exit(1);
+// } else {
     deleteCommand(name);
-}
+// }
