90e8b46052b58fcdc1fd37675f107f2433d0ac85
kirjavascript <snkenjoi@gmail.com>
2019-08-11 17:03:14 +0100

    added syntax highlighting

--- vim-plugin/list.js
+++ vim-plugin/list.js
@@ -2,6 +2,6 @@ const { getAllCommands } = require('../database/commands');
 getAllCommands()
     .forEach(({ name, locked, starred }) => {
         console.log(
-            `${name.padEnd(20)} ${starred ? '★' : ' '} ${locked ? '🔒' : ''}`
+            `${name.padEnd(43)} ${starred ? '★' : ' '} ${locked ? '🔒' : ''}`
         );
     });
