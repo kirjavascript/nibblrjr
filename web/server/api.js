6a59848e0209376e70eaceef68f488db2004c3d4
kirjavascript <snkenjoi@gmail.com>
2019-08-13 23:16:30 +0100

    vim apis for set, delete

--- web/server/api.js
+++ web/server/api.js
@@ -3,28 +3,45 @@ function initAPI({ parent, app }) {
 
     app.use('/api/*', (req, res, next) => {
         req.isAdmin = req.body.password === parent.web.password;
+        res.getCommand = (name) => {
+            const info = commands.get(name);
+            if (!info) {
+                res.json({ error: 'no such command' + name })
+            }
+            return info;
+        };
         next();
     });
 
     app.get('/api/command/get/:name', (req, res) => {
         const { name } = req.params;
-        const command = commands.get(name);
+        const info = res.getCommand(name);
         if ('raw' in req.query) {
             res.set('Content-Type', 'text/plain')
-                .send(command.command);
+                .send(info.command);
         } else {
-            res.json(command || { error: 'no such command ' + name });
+            res.json(info);
         }
     });
 
     app.post('/api/command/set/:name', (req, res) => {
         const { name } = req.params;
         const { command } = req.body;
-        const info = commands.get(name);
-        if (!info) {
-            res.json({ error: 'no such command' + name })
-        } else if (!info.locked || req.isAdmin) {
+        const info = res.getCommand(name);
+        if (!info.locked || req.isAdmin) {
             commands.set(name, command);
+            res.json({});
+        } else {
+            res.json({ error: 'no access' });
+        }
+    });
+
+    app.post('/api/command/delete/:name', (req, res) => {
+        const { name } = req.params;
+        const { command } = req.body;
+        const info = res.getCommand(name);
+        if (!info.locked || req.isAdmin) {
+            commands.delete(name);
             res.json({});
         } else {
             res.json({ error: 'no access' });
