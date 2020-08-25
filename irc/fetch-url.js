90320c20246d45d08dc7238d8e9b1ddcbdb216da
kirjavascript <snkenjoi@gmail.com>
2020-08-25 19:32:03 +0200

    changed user agent to googlebot

--- irc/fetch-url.js
+++ irc/fetch-url.js
@@ -34,6 +34,7 @@ function fetchURL({ text, print, disableRedirect = false, showAll = false }) {
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                 'Accept-Language': 'en-GB,en;q=0.5',
                 'Content-Language': 'en-GB,en;q=0.5',
+                'User-Agent': 'Googlebot',
             },
             hostname: parsed.hostname,
             path: parsed.path,
