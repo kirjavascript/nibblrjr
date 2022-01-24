ca5a3c1c92586c96fd52083ce0a52086fc1e4e35
kirjavascript <snkenjoi@gmail.com>
2022-01-24 00:26:23 +0000

    assertNotEvil

--- database/sql.js
+++ database/sql.js
@@ -13,11 +13,6 @@ const parser = require('sqlite-parser');
 // handle nsting
 
 const connections = new Map();
-const types = [
-];
-const variants = [
-];
-const functions = [];
 
 const traverse = item => {
     if (Array.isArray(item)) {
@@ -25,43 +20,72 @@ const traverse = item => {
             traverse(child);
         }
     } else {
-        // object check valie
-        console.log(item.type, item.variant, item.name);
+        const { type, variant, name } = item;
+        if (variant === 'attach') {
+            throw new Error('ATTACH forbidden');
+        }
+        if (variant === 'detach') {
+            throw new Error('DETACH forbidden');
+        }
+        if (variant === 'pragma') {
+            throw new Error('PRAGMA disallowed');
+        }
+        if (type === 'identifier' && variant === 'function' && name.includes('extension')) {
+            throw new Error('forbidden function');
+        }
         for (const child of Object.values(item)) {
-            if (typeof child === 'object') {
+            if (typeof child === 'object' && child !== null) {
                 traverse(child);
             }
         }
     }
-
 };
 
 const assertNotEvil = (query) => {
     const ast = parser(query);
     traverse(ast.statement);
-
-    console.log(require('util').inspect(ast, {depth: 9}));
-    // traverse keys
-    // for each node, check type and varient
 };
 
+// SELECT command_trigger as command, count(command_trigger) as count
+// FROM (
+//     SELECT CASE WHEN instr(message, ' ') <> 0 THEN substr(message, 1, instr(message, ' ')-1) ELSE message END as command_trigger
+//     FROM log
+//     WHERE time BETWEEN date(?, '-1 month') AND date(?)
+//     AND command = 'PRIVMSG'
+// )
+// WHERE command <> ''
+// GROUP BY command
+// ORDER BY count DESC
+// LIMIT 10
+
+// PRAGma max_page_count;
+// SELECT ? FROM foo WHERE (pragma);
+
+// SELECT hexasd(1) FROM foo WHERE (pragma);
+
+// CREATE TABLE Bees (
+//   id integer PRIMARY KEY ON CONFLICT ROLLBACK AUTOINCREMENT,
+//   name varchar(50) NOT NULL UNIQUE,
+//   wings integer CONSTRAINT has_enough_wings CHECK (wings >= 2),
+//   legs integer CONSTRAINT too_many_legs CHECK (legs <= 6)
+// );
+
+//             CREATE TABLE IF NOT EXISTS foo (
+//                 idx INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+//                 message TEXT
+//             );
 
 
-assertNotEvil(`
-    SELECT command_trigger as command, count(command_trigger) as count
-    FROM (
-        SELECT CASE WHEN instr(message, ' ') <> 0 THEN substr(message, 1, instr(message, ' ')-1) ELSE message END as command_trigger
-        FROM log
-        WHERE time BETWEEN date(?, '-1 month') AND date(?)
-        AND command = 'PRIVMSG'
-    )
-    WHERE command <> ''
-    GROUP BY command
-    ORDER BY count DESC
-    LIMIT 10
+// INSERT INTO Bees (name, wings, legs) VALUES
+//   ('Dr. Bee', 2, 6);
 
-`);
+// SELECT *
+// FROM Bees b
+// WHERE b.wings = 2 AND b.legs = 6;
 
+// INSERT INTO Bees (name, wings, legs) VALUES
+//   ('Mr. Dragonfly', 4, 6),
+//   ('Nick', 0, 2);
 
 
 
