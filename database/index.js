64f21a1422bc9bf82dba3346d44967808ed660f4
kirjavascript <snkenjoi@gmail.com>
2022-02-08 00:32:13 +0000

    note about db runtime

--- database/index.js
+++ database/index.js
@@ -1,6 +1,3 @@
-const fs = require('fs');
-const SQLiteDatabase = require('better-sqlite3');
-
 const { createCommandDB } = require('./commands');
 const { createServerDB } = require('./server');
 const { useSQLDB, waitSQLClose } = require('./sql');
@@ -9,20 +6,13 @@ class Database {
     constructor(parent) {
         this.commands = createCommandDB(parent);
 
+        // references to db runtime are stored here,
+        // so flushing the require cache has no unintended consequences
+
         this.createServerDB = createServerDB;
 
         this.useSQLDB = namespace => useSQLDB(parent, namespace);
         this.waitSQLClose = waitSQLClose;
-    }
-
-    createDB(name, schema) {
-        const filename = __dirname + `/../storage/server/${name}.db`;
-        fs.openSync(filename, 'a');
-        const db = new SQLiteDatabase(filename);
-        db.pragma('journal_mode = WAL'); // only one connection at a time is made
-        db.exec(schema);
-        db.function('REGEXP', (a, b) => new RegExp(a, 'm').test(b) ? 1 : 0);
-        return db;
     }
 };
 
